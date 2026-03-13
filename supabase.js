1dc2d209969564a824bc33aefe9e08be181f5ab2
tutuxiaomao <2750304600@qq.com>
2026-03-13 19:12:18 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // supabase.js
 const SUPABASE_URL = 'https://gpdnhyonmvklhhhnsrpu.supabase.co';  
-const SUPABASE_ANON_KEY = 'sb_publishable_E3oR3sfXE_yXzSq7CqQcpw_SP_O5RRU';     
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwZG5oeW9ubXZrbGhoaG5zcnB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzOTE2MjcsImV4cCI6MjA4ODk2NzYyN30.sc2qQRFtQhJFuSXStqOmByCv-VZzu_0YzvYLHVLcphs';     
 
 const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
